d4d1428b48463967ddc67a6c07c1a2551d84a5f2
Matt Langford <matthew.langford95@gmail.com>
2024-04-06 11:32:59 -0400

    gif version

---
+++ copy_history.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Path to the file in the repository you want to copy
+FILE_PATH="cover.png"
+
+# Start and end commits of the range
+START_COMMIT=ef37ee811a4d94ae82a2e0803ee5f597ae7ddd47
+
+# Directory where you want to copy the files
+DEST_DIR="/tmp/cover/"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Counter for file naming
+COUNTER=0
+
+# Loop through each commit in the range
+for COMMIT_HASH in $(git rev-list --reverse $START_COMMIT^..); do
+  # Check out the file at the current commit
+  git checkout $COMMIT_HASH -- $FILE_PATH
+  
+  # Copy the file to the destination directory with a counter suffix
+  printf -v FORMATTED_COUNTER "%02d" $COUNTER
+  cp "$FILE_PATH" "$DEST_DIR/img_$FORMATTED_COUNTER.png"
+  
+  # Increment the counter
+  let COUNTER=COUNTER+1
+done
+
+echo "Files copied successfully."
